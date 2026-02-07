61db9063e2ac76644fa2bd2fcd13913d4a8f14ae
Hinks <henrik.nr10@gmail.com>
2026-02-07 13:38:00 +0100

    lsp diagnostic virtual lines

--- lua/hinks/lazy/lsp.lua
+++ lua/hinks/lazy/lsp.lua
@@ -4,6 +4,13 @@ return {
         config = function()
             vim.lsp.enable('elmls')
             vim.lsp.enable('ts_ls')
+
+            vim.diagnostic.config({ virtual_lines = true })
+
+            vim.keymap.set('n', 'gK', function()
+                local new_config = not vim.diagnostic.config().virtual_lines
+                vim.diagnostic.config({ virtual_lines = new_config })
+            end, { desc = 'Toggle diagnostic virtual_lines' })
         end,
     }
 }
